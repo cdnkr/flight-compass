019e19e21aa95af32064e319abed287c987d2cff
cdnkr <chad@forgood.co.za>
2024-11-13 20:19:29 +0200

    adds refresh button and removes polling

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react'
 import FlightCompass from './flight-compass'
 import { Map } from './map'
 import { Planes } from './planes'
-import { X } from 'lucide-react'
+import { Compass, RefreshCcw, X } from 'lucide-react'
 
 const PLANS_WITHIN_RADIUS_KM = 500
 
@@ -26,22 +26,24 @@ export default function Main() {
     requestPermission: requestGeolocationPermission,
   } = useGeolocation()
   const [selectedFlight, setSelectedFlight] = useState<Flight | null>(null)
+  const [lastFetch, setLastFetch] = useState<Date | null>(null)
+
+  async function fetchPlanes() {
+    if (!position) return
+
+    const fetchedPlanes = await getFlights({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      radiusKM: PLANS_WITHIN_RADIUS_KM,
+    })
+
+    console.log(fetchedPlanes)
+    setLastFetch(new Date())
+
+    if (Array.isArray(fetchedPlanes)) setFlights([...fetchedPlanes])
+  }
 
   useEffect(() => {
-    async function fetchPlanes() {
-      if (!position) return
-
-      const fetchedPlanes = await getFlights({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        radiusKM: PLANS_WITHIN_RADIUS_KM,
-      })
-
-      console.log(fetchedPlanes)
-
-      if (Array.isArray(fetchedPlanes)) setFlights([...fetchedPlanes])
-    }
-
     if (
       geolocationPermission === 'granted' &&
       position &&
@@ -49,10 +51,6 @@ export default function Main() {
     ) {
       fetchPlanes()
     }
-
-    setInterval(() => {
-        fetchPlanes()
-    }, 60000); // Update every minute
   }, [geolocationPermission, position])
 
   async function onRequestPermission() {
@@ -62,10 +60,10 @@ export default function Main() {
 
   return (
     <div className="p-4 max-w-screen-sm mx-auto w-full">
-      <div className="mb-2 md:mb-6">
+      <div className="mb-2 md:mb-4">
         <h1 className="text-2xl font-bold uppercase">Flight Compass</h1>
         <p className="text-gray-400 text-sm uppercase">
-          Planes within {PLANS_WITHIN_RADIUS_KM}km. Data updates every minute. {process.env.NEXT_PUBLIC_USE_MOCK_DATA && <i className='lowercase text-orange-500'>(using mock data)</i>}
+          Planes within {PLANS_WITHIN_RADIUS_KM}km. {process.env.NEXT_PUBLIC_USE_MOCK_DATA === 'true' && <i className='lowercase text-orange-500'>(using mock data)</i>}
         </p>
         {!hasDeviceOrientationSupport && (
           <div className="mt-2">
@@ -77,12 +75,27 @@ export default function Main() {
           </div>
         )}
       </div>
+      {(compassPermission === 'granted') && (
+        <>
+          <button
+            onClick={fetchPlanes}
+            className="p-2 w-full mb-2 cursor-pointer flex items-center justify-center gap-2 bg-blue leading-none text-center uppercase text-sm font-mono"
+          >
+            <RefreshCcw className="size-4" />Refresh Planes
+          </button>
+          {lastFetch && (
+            <p className="text-sm text-gray-400 mb-8">
+              Last updated {lastFetch.toLocaleString()}
+            </p>
+          )}
+        </>
+      )}
       {compassPermission !== 'granted' ? (
         <button
           onClick={onRequestPermission}
-          className="mb-4 px-4 py-2 uppercase bg-blue text-white rounded hover:bg-blue-600"
+          className="p-2 w-full cursor-pointer flex items-center justify-center gap-2 bg-blue leading-none text-center uppercase text-sm font-mono"
         >
-          Enable Compass and Location
+          <Compass className="size-4" />Enable Compass and Location
         </button>
       ) : (
         <FlightCompass
